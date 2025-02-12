3b8b1cbba439fe171b99a64951d5aece950a266e
wuchenxiuwu <116479750+wuchenxiuwu@users.noreply.github.com>
2025-02-12 23:24:24 +0800

    更新 PopupMenuWindow.java
    大砍，原有的检查更新减去了不必要的因素

--- app/src/main/java/com/leohao/android/alistlite/window/PopupMenuWindow.java
+++ app/src/main/java/com/leohao/android/alistlite/window/PopupMenuWindow.java
@@ -16,36 +16,31 @@ import com.leohao.android.alistlite.R;
 public class PopupMenuWindow extends PopupWindow {
     public PopupMenuWindow(Context context) {
         super(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        //再次点击菜单时隐藏菜单
+        // 再次点击菜单时隐藏菜单
         setOutsideTouchable(true);
         setFocusable(true);
         View inflate = LayoutInflater.from(context).inflate(R.layout.popup_menu_view, null);
         setContentView(inflate);
-        //设置窗口进入和退出的动画
+        // 设置窗口进入和退出的动画
         setAnimationStyle(R.style.PopupMenuWindowStyle);
-        //定义点击事件监听
+        // 定义点击事件监听
         View popupView = getContentView();
-        //权限配置
+        // 权限配置
         popupView.findViewById(R.id.btn_startPermissionCheckActivity).setOnClickListener((view) -> {
             dismiss();
             MainActivity.getInstance().startPermissionCheckActivity(view);
         });
-        //密码设置
+        // 密码设置
         popupView.findViewById(R.id.btn_setAdminPassword).setOnClickListener((view) -> {
             dismiss();
             MainActivity.getInstance().setAdminPassword(view);
         });
-        //更新日志
-        popupView.findViewById(R.id.btn_showReleaseLog).setOnClickListener((view) -> {
-            dismiss();
-            MainActivity.getInstance().showReleaseLog(view);
-        });
-        //高级配置
+        // 高级配置
         popupView.findViewById(R.id.btn_manageConfigData).setOnClickListener((view) -> {
             dismiss();
             MainActivity.getInstance().manageConfigData(view);
         });
-        //关于 AList
+        // 关于 AList
         popupView.findViewById(R.id.btn_showSystemInfo).setOnClickListener((view) -> {
             dismiss();
             MainActivity.getInstance().showSystemInfo(view);
